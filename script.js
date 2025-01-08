be447e1d502e5ae72742f07f7a6b6faa4f318023
idatol <ida.k.tollaksen@hiof.no>
2025-01-08 13:55:17 +0100

    startet på css og et par småendringer

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
 // Velg <main> for å legge til seksjoner
 const main = document.querySelector("main");
 
+// Koden under oppretter seksjoner for hver kategori 
+// dynamisk istedenfor å skrive dem manuelt i HTML.
 // Iterer gjennom dataene i ressurser.js
 resources.forEach(resource => {
     // Lag en seksjon for hver kategori
     const section = document.createElement("section");
+    // Sett id til seksjonen som passer til kategorien
     section.id = resource.category;
 
-    // Legg til tittel
+    // Legg til tittel som h1 element
     const title = document.createElement("h1");
     title.textContent = resource.category;
     section.appendChild(title);
 
-    // Legg til beskrivelse
+    // Legg til beskrivelse som p element
     const description = document.createElement("p");
     description.textContent = resource.text;
     section.appendChild(description);
@@ -27,8 +30,9 @@ resources.forEach(resource => {
         listItem.appendChild(link);
         sourceList.appendChild(listItem);
     });
+    // Legg til listen i seksjonen
     section.appendChild(sourceList);
 
-    // Legg til seksjonen i main
+    // Legg til en og en seksjonen i main
     main.appendChild(section);
 });
